962b1192b68ba3ef31f4aa6aff0f860a711fa697
mauryrodriguez <masu_lm@hotmail.com>
2019-04-16 19:35:47 -0500

    segundo ejercicio

---
+++ ejercicio2/ejerciciodos.php
@@ -0,0 +1,19 @@
+<?php
+
+function titulo(){
+echo 'Hola mundo';
+}
+?>
+<!DOCTYPE  html>
+<html>
+
+<body>
+
+<div align='center'> <?php titulo() ?></div>
+<br>
+<div align='center'> <?php titulo() ?></div>
+<br>
+<div align='center'> <?php titulo() ?></div>
+</body>
+
+</html>
